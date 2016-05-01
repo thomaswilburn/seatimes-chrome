de4fffd9cb4324f0fcb18a9fbc041be36b507ac1
Thomas Wilburn <news@thomaswilburn.net>
2016-04-30 23:27:07 -0700

    Watch task created

--- scripts/watch.js
+++ scripts/watch.js
@@ -1,3 +1,34 @@
-var glob = require("glob");
+var gaze = require("gaze");
 var spawn = require("child_process").spawn;
+var chalk = require("chalk");
 
+var debounce = function(f, delay) {
+  var timeout = null;
+  return function() {
+    if (timeout) return;
+    timeout = setTimeout(function() {
+      timeout = null;
+      f();
+    }, delay || 100);
+  }
+};
+
+var npm = process.platform == "win32" ? "npm.cmd" : "npm";
+
+var watch = function(name, pattern, scripts) {
+  console.log(chalk.bgBlue.white("Starting watch task: " + name));
+  gaze(pattern, { debounceDelay: 200 }, function(err, watcher) {
+    watcher.on("all", function() {
+      console.log(chalk.bgBlue.white(name + " - file change detected"));
+      var p = spawn(npm, ["run"].concat(scripts), { stdio: "inherit" });
+      p.on("close", function() {
+        console.log(chalk.bgBlue.white(name + " finished"));
+      });
+    });
+  });
+};
+
+//Set watches
+watch("Browserify", "src/js/**/*", ["bundle"]);
+watch("Copy", "src/index.html", ["copy"]);
+watch("LESS", "src/less/**/*", ["less"]);
