7f50331ece1fb464870988c20b9be7eaa25bac5a
Thomas Wilburn <news@thomaswilburn.net>
2016-05-01 23:50:39 -0700

    Update docs

--- src/js/worker/seatimes.js
+++ src/js/worker/seatimes.js
@@ -4,11 +4,9 @@ This module handles loading content, either from the network or the database cac
 
 Request flow:
 - App asks for content X
-- Check the database to see if we have it.
-- If we do, return that immediately.
-- Also fire off a network request to check for updates.
-- If the network had a newer copy, fire an "update" event for the app.
-- If we didn't have this, return the new content to the app
+- Check the database to see if we have it AND fire off a network request for it
+- Don't wait for the network if the database already has it
+- If it was cached, but the network had a newer copy, fire an "update" event for the app.
 - Cache the result of the network call in the database for next request.
 
 */
